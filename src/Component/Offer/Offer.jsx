9352c2ac119b3a125b88ac967e939b2fb70685fa
jannatun-nur <jannatunur03@gmail.com>
2024-12-02 10:14:09 +0600

    offer 60%

--- src/Component/Offer/Offer.jsx
+++ src/Component/Offer/Offer.jsx
@@ -3,7 +3,7 @@ import offer from '../../../public/images/offer.png'
 import './offer.css'
 const Offer = () => {
     return (
-        <div>
+        <div className='bg-white min-h-screen'>
       <div>
       <img  
             className='lg:ml-60'
@@ -11,7 +11,7 @@ const Offer = () => {
              src={holiday}/>
       </div> 
 
-            <div className='flex justify-between px-10'>
+            <div className='lg:flex lg:justify-between px-10'>
                <section>
                <img src={offer}
                 id='offer'  />
@@ -20,9 +20,10 @@ const Offer = () => {
                 <section 
                 id="offer-text"
                 className='text-black '>
-                <p>Extra beef mashroom chasse platter</p>
-                <p>40.98 $ ONLY</p>
-                <button>ORDER NOW</button>
+                <p className='lg:text-3xl text-[#027361] font-serif'>Extra beef mashroom chasse platter</p>
+                <p className='lg:text-3xl text-[#027361] font-serif py-4'>40.98 $ <span className='text-[#f57d11]
+                font-bold font-serif shadow-slate-50'> ONLY</span></p>
+                <button className='lg:text-4xl font-bold text-[#027361] animate-bounce font-serif py-6'>ORDER <span  className='text-[#f57d11]'>NOW</span> </button>
 
                 </section>
             </div>
