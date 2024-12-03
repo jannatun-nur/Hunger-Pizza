44126b3de8ce84d4db7f295a06d22071a88e3671
jannatun-nur <jannatunur03@gmail.com>
2024-12-03 18:16:07 +0600

    review 20%

--- src/Component/Review/Review.jsx
+++ src/Component/Review/Review.jsx
@@ -1,9 +1,23 @@
+import { useEffect, useState } from "react";
+import ReviewCard from "./ReviewCard";
 
 
 const Review = () => {
+
+    const [review , setReview] = useState([])
+
+    useEffect( ()=>{
+        fetch('Review.json')
+        .then( res => res.json())
+        .then(data =>setReview(data))
+    },[])
     return (
         <div>
-            
+            <div>
+                {
+                    review.map( reviews =><ReviewCard key={reviews} reviews={reviews}></ReviewCard>)
+                }
+            </div>
         </div>
     );
 };
